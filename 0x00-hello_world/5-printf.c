7ca472b4fac9cf937988567ba968db8dd3229f19
Mary Patience <nmarypatience@gmail.com>
2022-09-08 15:46:15 -0700

    prepare

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- * main-prints the phrase
- * return 0 if exited properly
- * */
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(){
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
