e7cf08a72eebfc3e3708c4a558d764b6c067a848
Mary Patience <nmarypatience@gmail.com>
2022-09-08 15:41:29 -0700

    prepare

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
  * return 0 if exited properly
  * */
 int main(){
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return o;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
