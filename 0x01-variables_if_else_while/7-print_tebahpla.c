343be8dd2a36c07a69de4cdcedc6bbbbbe7f0b90
Mary Patience <nmarypatience@gmail.com>
2022-09-11 07:44:47 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+nclude <stdio.h>
+/**
+ * main - prints value of "greater than 5" or " zero" or "than 6 and not 0"
+ *
+ * Description: if else statement
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	int ch;
+	for (ch = 'z'; ch >= 'a' ; ch--)
+	{
+		putchar (ch);
+	}
+	putchar ('\n');
+	return (0);
+}
