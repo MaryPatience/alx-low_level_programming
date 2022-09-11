bd7bf38f8271ae7c1f4cf0a4836cb6d91fcf65cd
Mary Patience <nmarypatience@gmail.com>
2022-09-11 08:27:09 -0700

    order

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - base 10 with putchar function
+ *
+ * Description: base 1o
+ * Return: 0 when it runs as expected
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 10; ch++)
+	{
+		putchar(ch + '0');
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
