7e938864fe6bc4d7f7ba19a6a885470926976037
Mary Patience <nmarypatience@gmail.com>
2022-09-11 07:17:34 -0700

    put

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - base 10 with putchar function
+ *
+ * Description: base 1o
+ * Return: 0 when it runs as expected
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 10; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
