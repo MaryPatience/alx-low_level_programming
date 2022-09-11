d0d36694a0311b9d78311d69915a0ddefd211556
Mary Patience <nmarypatience@gmail.com>
2022-09-11 06:23:29 -0700

    putchar alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints value of "greater than 5" or " zero" or "than 6 and not 0"
+ *
+ * Description: if else statement
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	int ch;
+	
+	for (ch = 97; ch < 123 ; ch++)
+	{
+		putchar (ch);
+	}
+	for (ch = 'A'; ch <= 'Z' ; ch++)
+	{
+		putchar (ch);
+	}
+       	putchar ('\n');
+	return (0);
+}
