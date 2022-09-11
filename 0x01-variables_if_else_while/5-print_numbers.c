63b0641184c08954fef6310d39cce9915597e7a8
Mary Patience <nmarypatience@gmail.com>
2022-09-11 06:55:50 -0700

    base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - base 10
+ *
+ * Description: for
+ * Return: 0 when it runs as expected
+ */
+int main(void)
+{
+	int x;
+
+	for(x=0; x < 10; x++)
+	{
+		print("%d", x);
+	}
+	return (0);
+}
+
