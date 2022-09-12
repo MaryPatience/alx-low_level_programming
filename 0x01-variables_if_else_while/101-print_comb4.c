5996dbd815720506db5ccdec69dfe90afd122bb4
Mary Patience <nmarypatience@gmail.com>
2022-09-12 12:26:46 -0700

    3-var

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - base 10 with putchar function
+ *
+ * Description: base 1o
+ * Return: 0 when it runs as expected
+ */
+int main(void)
+{
+	int ch;
+	int vr;
+	int xy;
+
+	for (ch = 0; ch < 8; ch++)
+	{
+		for (vr = ch + 1; vr < 9; vr++)
+		{
+			for (xy = ch + 2; xy < 10; xy++)
+			{	
+			putchar(ch + '0');
+			putchar(vr + '0');
+			putchar(xy + '0');
+			if (ch == 7 && vr == 8 && xy == 9)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
