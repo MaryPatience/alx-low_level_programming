8fa497c425162a7bd12e7f4ddc6be109119ebe28
Mary Patience <nmarypatience@gmail.com>
2022-09-12 13:20:24 -0700

    3-var

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,6 +9,8 @@ int main(void)
 {
 	int ch;
 	int vr;
+	int m;
+	int n;
 
 	for (ch = 0; ch < 10; ch++)
 	{
@@ -16,6 +18,13 @@ int main(void)
 		{
 			putchar(ch + '0');
 			putchar(vr + '0');
+			putchar(32);
+			for (m = 0; m < 10; m++)
+			{
+				for  (n = m + 1; n < 10; n++)
+				{
+					putchar(m + '0');
+					putchar(n + '0');
 			if (ch == 9 && vr == 9)
 			{
 				break;
