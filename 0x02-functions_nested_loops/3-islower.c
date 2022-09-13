1f3828420275e1eb1b12119e0cf337508562069c
Mary Patience <nmarypatience@gmail.com>
2022-09-13 11:29:25 -0700

    check lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * _islower - nested loops
+ *
+ * Description: putchar
+ * Return: 0 when it runs as expected
+ */
+int _islower(int c);
+{
+	int c;
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
