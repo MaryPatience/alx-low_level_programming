179d2630367ca4ed1fe7730b47b8405da3aef317
Mary Patience <nmarypatience@gmail.com>
2022-09-14 14:37:46 -0700

    check lower

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - nested loops
+ *
+ * Description: putchar
+ * Return: 0 when it runs as expected
+ */
+int _isalpha(int c)
+{
+	        if ((c > 'a' && c <= 'z') || (c > 'A' && c <= 'Z'))
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+}
