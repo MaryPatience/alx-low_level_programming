c59e26e36a8858495fd500e1223434ffe61a6a78
Mary Patience <nmarypatience@gmail.com>
2022-09-15 01:28:40 -0700

    check lower

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - sum up
+ * @a: first integer
+ * @b \: seconf integder
+ *
+ * Description: adder
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
