d43c66c506a69190c61f96a04603d66b65b8cef4
Mary Patience <nmarypatience@gmail.com>
2022-09-14 22:59:05 -0700

    variables

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - nested loops
+ * @c: the int to print
+ *
+ * Description: putchar
+ * Return: 0 when it runs as expected
+ */
+int print_last_digit(int c)
+{
+	return (c % 10);
+}
