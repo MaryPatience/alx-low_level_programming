3a61109db568ffcea4b566fb84df6d2447761556
Mary Patience <nmarypatience@gmail.com>
2022-09-15 00:30:22 -0700

    check lower

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * times_table - multiplication table of 9
  *
@@ -14,7 +15,7 @@ void times_table(void)
 		for (cone =0; cone <=9; cone++)
 		{
 			d = cone * rone;
-			_putchar(d + '0');
+			putchar(d + '0');
 			if (cone == 9 && rone == 9)
 			{
 				break;
