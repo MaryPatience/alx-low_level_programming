b07177cc6ee756adfdef57682d8bc84365d8e9af
Mary Patience <nmarypatience@gmail.com>
2022-09-14 22:45:31 -0700

    variables

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <math.h>
+/**
+ * _abs - nested loops
+ * @c: the int to print
+ *
+ * Description: putchar
+ * Return: 0 when it runs as expected
+ */
+int _abs(int)
+{
+	int c;
+
+	putchar('abs(c)' + '0');
+	return (0);
+}
