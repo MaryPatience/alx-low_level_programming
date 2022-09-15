6be6e20fdfa953d04f8ba490511c27cbafd7a704
Mary Patience <nmarypatience@gmail.com>
2022-09-15 01:42:31 -0700

    check lower

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_to_98 - function
+ * @n: variable
+ *
+ * Description: natural numbers
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int ch;
+
+	if (n > 98)
+	{
+		for (ch = n; ch >= 98; ch--)
+		{
+			_putchar(ch + '0');
+		}
+	}	
+	else if (n == 98)
+	{
+		_putchar(98 + '0');
+	}
+	else
+	{
+		for (ch = n; ch <= 98; ch++)
+		{
+			 _putchar(ch + '0');
+		}
+	}
+}
+
