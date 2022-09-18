21e424894df093cdaf1b6ebe9034f48605c9fb00
Mary Patience <nmarypatience@gmail.com>
2022-09-18 03:09:56 -0700

    upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - capital letters
+ * @c: variable
+ *
+ * Description: show us capital letters
+ * Return: 0  well run successfully
+ */
+int _isupper(int c)
+{
+	if  (c >='A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}	
