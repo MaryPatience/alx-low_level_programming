a862eb5f8303115d42973c0f61d68bb1fde21ae8
Mary Patience <nmarypatience@gmail.com>
2022-09-18 04:48:57 -0700

    product

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - capital letters
+ *
+ * Description: show us capital letters
+ * Return: 0  well run successfully
+ */
+void print_numbers(void)
+{
+	int x;
+
+	if (x >= '0' && x >= '9')
+	{
+		_putchar(x + '0');
+	}
+	_putchar('\n');
+	return (0);
+}
