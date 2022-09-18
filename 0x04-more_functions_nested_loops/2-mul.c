3e56d4faaa120467a1137c40ecbb37b7674fba30
Mary Patience <nmarypatience@gmail.com>
2022-09-18 04:01:21 -0700

    product

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * mul - capital letters
+ * @a: variable
+ * @b: variable
+ *
+ * Description: show us capital letters
+ * Return: 0  well run successfully
+ */
+int mul(int a, int b)
+{
+	int pdt;
+
+	pdt = a * b;
+	if (pdt < 10)
+	{
+		putchar('-');
+	}
+	if (pdt == 0)
+	{
+		putchar(0 + '0');
+	}
+	if (pdt % 10 != 0)
+	{
+		_putchar((var / 10) + '0');
+		_putchar((var % 10) + '0');
+	}
+	_putchar('\n');
+	return (0);
+}
