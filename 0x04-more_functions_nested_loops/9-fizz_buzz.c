5174c97abc86cc825d295cb87a8056ba6fe8968e
Mary Patience <nmarypatience@gmail.com>
2022-09-18 14:02:45 -0700

    buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,29 +10,29 @@
 
 int main(void)
 {
-	int c;
+	int i;
 
-	for (c = 1; c <= 100; c++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (c % 3 == 0 && c % 5 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz ");
 		}
-		else if (c % 3 == 0 && c % 5 != 0) 
+		else if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf("Fizz ");
 		}
-		else if (c % 5 == 0 && c % 3 != 0)
+		else if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf("Buzz ");
 		}
-		else if (c == 1)
+		else if (i == 1)
 		{
-			printf("%d", c);
+			printf("%d", i);
 		}
 		else
 		{
-			printf("%d ", c);
+			printf("%d ", i);
 		}
 	}
 	printf("\n");
