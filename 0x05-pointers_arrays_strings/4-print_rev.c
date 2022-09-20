ce2df9453c55ea370e9145a174611534f0603a70
Mary Patience <nmarypatience@gmail.com>
2022-09-20 15:21:05 -0700

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -16,11 +16,11 @@ void print_rev(char *s)
 	len = 0;
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		c++;
+		len++;
 	}
-	for (i = c - 1; i >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 	{
-		_putchar(*(p + i));
+		_putchar(*(s + i));
 	}
 	_putchar('\n');
 }
