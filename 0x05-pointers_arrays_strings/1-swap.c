669eb953256ae75f789081b14c4038234d362b8c
Mary Patience <nmarypatience@gmail.com>
2022-09-20 11:52:52 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * swap_int - reseting
+ * @*a: our pointer
+ * @a: the actual parameter
+ * @*b: our pointer
+ * @b: our parameter
+ *
+ * Description: change its value
+ * Return: 0 Always
+ */
+void swap_int(int *a, int *b);
+{
+	int x;
+	int y;
+	int temp;
+
+	a = &x;
+	b = &y;
+	temp = *b;
+	*b = *a;
+	*a = temp;
+}
