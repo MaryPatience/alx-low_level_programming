01aa0046fd526546574ebb2c82ec76f8627bf3dd
Mary Patience <nmarypatience@gmail.com>
2022-09-20 16:37:59 -0700

    reverse

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,6 +13,13 @@ void puts2(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(*(str + i + 1));
+		if (i % 2 != 0)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(*(str + i));
+		}
 	}
 }
