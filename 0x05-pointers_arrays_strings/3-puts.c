efe9101516d522122b9585e3016e8b2a91f0f004
Mary Patience <nmarypatience@gmail.com>
2022-09-20 14:03:49 -0700

    length

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts - reseting
+ * @*str: our pointer
+ * @str: the actual parameter
+ *
+ * Description: change its value
+ * Return: 0 Always
+ */
+void _puts(char *str);
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		_putchar(s[count]);
+		count++;
+		s++;
+	}
+}
