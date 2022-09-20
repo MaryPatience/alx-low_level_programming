d3a24006be3d354e846e922de62e3e4275ef877c
Mary Patience <nmarypatience@gmail.com>
2022-09-20 11:39:21 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - reseting
+ * @*n: our pointer
+ * @n: the actual parameter
+ *
+ * Description: change its value
+ * Return: 0 Always
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
