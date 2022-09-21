90880ce00b225714746a555b2a108ec5fa7d1ae4
Mary Patience <nmarypatience@gmail.com>
2022-09-20 17:06:51 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * puts_half - reseting
+ * @*str: our pointer
+ * @str: the actual parameter
+ *
+ * Description: change its value
+ * Return: 0 Always
+ */
+void puts_half(char *str)
+{
+	int i;
+	int len;
+	int n;
+	int j;
+
+	len = 0;
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+	}
+	else
+	{
+		n = (length_of_the_string - 1) / 2;
+	}
+	for (j = n; str[j] != '\0'; j++)
+	{
+		_putchar(*(str + j));
+	}
+	_putchar('\n');
+}
