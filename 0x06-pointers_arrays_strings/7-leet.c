06ffb53a6b7ae0e685709b4c13d22a1f6126be77
Mary Patience <nmarypatience@gmail.com>
2022-09-23 15:42:00 -0700

    coding

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *leet - concatenator
+ * @*str: string 1
+ * @str: string 1
+ *
+ * Description: join 2 strings
+ * Return: Always 0
+ */
+char *leet(char *str)
+{
+	int index1 = 0, index2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?' 'T'};
+
+	while (str[++index1])
+	{
+		for (index2 = 0; index2 <= 7; index2++)
+		{
+			if (str[index1] == leet[index2] ||
+					str[index1] - 32 == leet[index2])
+			{
+				str[index1] = index2 + '0';
+			}
+		}
+	}
+	return (str);
+}
