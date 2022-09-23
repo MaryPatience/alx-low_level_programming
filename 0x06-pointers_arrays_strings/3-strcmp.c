6fceb0730529514312789e0b8cea26e7677dc390
Mary Patience <nmarypatience@gmail.com>
2022-09-23 12:47:57 -0700

    compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * *_strcmp - concatenator
+ * @*s1: string 1
+ * @s1: string 1
+ * @*s2: string 2
+ * @s2: string 2
+ *
+ * Description: join 2 strings
+ * Return: Always 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, diff = 0;
+
+	while (1)
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+		{
+			break;
+		}
+		else if (s1[i] == '\0')
+		{
+			diff = s2[i];
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			diff = s1[i];
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+		}
+		else
+		{
+			i++;
+		}
+	}
+	return (diff);
+}
